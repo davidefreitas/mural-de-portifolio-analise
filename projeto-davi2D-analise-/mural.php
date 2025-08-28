820681b4190f9386de69f6ced3e9dd1483f4faaa
davidefreitas <davi.conceicao.freitas@escola.pr.gov.br>
2025-08-28 10:46:44 -0300

    Add files via upload

---
+++ projeto-davi2D-analise-/mural.php
@@ -0,0 +1,22 @@
+<?php
+include "conexao.php";
+if(isset($_POST['cadastra']))
+    $nome  = mysqli_real_escape_string($conexao, $_POST['nome']);
+    $email = mysqli_real_escape_string($conexao, $_POST['email']);
+    $msg   = mysqli_real_escape_string($conexao, $_POST['msg']);
+
+    $sql = "INSERT INTO recados (nome, email, mensagem) VALUES ('$nome', '$email', '$msg')";
+    mysqli_query($conexao, $sql) or die("Erro ao inserir dados: " . mysqli_error($conexao));
+    header("Location: mural.php");
+    exit;
+    if(isset($_POST['cadastra'])){
+        $nome  = mysqli_real_escape_string($conexao, $_POST['nome']);
+        $email = mysqli_real_escape_string($conexao, $_POST['email']);
+        $msg   = mysqli_real_escape_string($conexao, $_POST['msg']);
+    
+        $sql = "INSERT INTO recados (nome, email, mensagem) VALUES ('$nome', '$email', '$msg')";
+        mysqli_query($conexao, $sql) or die("Erro ao inserir dados: " . mysqli_error($conexao));
+        header("Location: mural.php");
+        exit;
+    }
+    ?>
