251c95dd51ffb1e665ee06edf22ba10381836294
davidefreitas <davi.conceicao.freitas@escola.pr.gov.br>
2025-09-05 16:03:43 -0300

    Add files via upload

---
+++ portifolio-davi2d-analise/cabeçalho.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8"/>
+<title>Mural de pedidos</title>
+<link rel="stylesheet" href="style.css"/>
+<script src="scripts/jquery.js"></script>
+<script src="scripts/jquery.validate.js"></script>
+</head>
+<body>
+<div id="main">
+<div id="geral">
+<div id="header">
+    <h1>Mural de pedidos</h1>
+</div>
